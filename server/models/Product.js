457fb6da5a8ff6c2833dcd2286689d86361b8280
HelderLessa <helderlssande@gmail.com>
2025-05-07 15:33:10 -0300

    Versão inicial do projeto 4

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,20 +1,16 @@
 import mongoose from "mongoose";
-import { loadType } from "mongoose-currency";
 
 const Schema = mongoose.Schema;
-loadType(mongoose);
 
 const ProductSchema = new Schema(
   {
     price: {
-      type: mongoose.Types.Currency,
-      currency: "USD",
-      get: (v) => v / 100,
+      type: mongoose.Schema.Types.Decimal128,
+      get: (v) => parseFloat(v.toString()),
     },
     expense: {
-      type: mongoose.Types.Currency,
-      currency: "USD",
-      get: (v) => v / 100,
+      type: mongoose.Schema.Types.Decimal128,
+      get: (v) => parseFloat(v.toString()),
     },
     transactions: [
       {
